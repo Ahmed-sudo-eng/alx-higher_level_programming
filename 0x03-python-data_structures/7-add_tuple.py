a0264a50bc7fb2028a50ae6f9958829527c5649f
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-05 13:07:27 +0200

    7-add_tuple.py file created

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) >= 2:
+        a_0, a_1 = tuple_a[0], tuple_a[1]
+    elif len(tupe_a) == 1:
+        a_0, a_1 = tuple_a[0], 0
+    elif len(tupe_a) == 0:
+        a_0, a_1 = 0, 0
+
+    if len(tuple_b) >= 2:
+        b_0, b_1 = tuple_b[0], tuple_b[1]
+    elif len(tuple_b) == 1:
+        b_0, b_1 = tuple_b[0], 0
+    elif len(tuple_b) == 0:
+        b_0, b_1 = 0, 0
+
+    c_0 = a_0 + b_0
+    c_1 = a_1 + b_1
+    tuple_c = (c_0, c_1)
+    return tuple_c
