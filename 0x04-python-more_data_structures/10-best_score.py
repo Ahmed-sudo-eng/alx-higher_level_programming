b4de2249f7602fe8eb572800635c8f1bb17497b3
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-08 20:08:31 +0200

    10-best_score.py file modified

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,11 @@ def best_score(a_dictionary):
     if a_dictionary == None:
         return None
     b_score = 0
+    key_with_best_score = None
     for score in a_dictionary.values():
         if score > b_score:
             b_score = score
     for key, value in a_dictionary.items():
         if value == b_score:
-            return key
+            key_with_best_score = key
+    return key_with_best_score
