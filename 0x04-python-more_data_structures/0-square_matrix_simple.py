0667e34a3509945fb58ba292e43d1bdb1fe2ffd4
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-08 15:49:47 +0200

    0-square_matrix_simple.py file created

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(element ** 2)
-        new_matrix.append(new_row)
+    new_matrix = [[x**2 for x in row] for row in matrix]
     return new_matrix
