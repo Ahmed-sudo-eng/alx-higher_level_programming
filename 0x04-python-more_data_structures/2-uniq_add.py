4d2138230fdd30b42874450a33cc7bdb10aed2a1
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-08 18:10:46 +0200

    2-uniq_add.py file modified

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    tmp_list = []
+    my_set = set(my_list)
     result = 0
-    for i in my_list:
-        if i in tmp_list:
-            pass
-        else:
-            result += i
-        tmp_list.append(i)
+    for i in my_set:
+        result += i
     return result
