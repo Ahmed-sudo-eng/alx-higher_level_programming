28106160208685d2bb1c5cae48c6d0c09c0f5047
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-20 20:06:10 +0200

    101-saf..., 102-magic..., 103-pytho... files were created

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as ve:
-        sys.stderr.write("Exception: {}".format(ve))
+        sys.stderr.write("Exception: {}\n".format(ve))
         return False
     else:
         return True
