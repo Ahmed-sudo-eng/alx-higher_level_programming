a61c2578a80790556ff5e419b0feaa024629b119
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-21 18:13:07 +0200

    2-square.py file was created

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This Module defines a class Square by private instance attribute size """
+
+
+class Square:
+    """Square class defines a square by size """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,2 +1,3 @@
 0-square.py: an empty class Square that defines a a square
 1-square.py: a class Square that defines a square by: size
+2-square.py: a class Square that defines a square by: size
