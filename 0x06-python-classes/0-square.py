0a04b16082cc6ed1dead5dd2a347f7e5ace1a222
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-21 16:53:13 +0200

    0-square.py file was modified

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module have an empty class square that defines a square
+"""
 
 class Square():
+    """ An empty class Square that defines a square
+        Attribute:
+        Nothing
+    """
+
     pass
