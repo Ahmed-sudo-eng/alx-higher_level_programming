c70ae0169f7e36003e8849b774956d63d7cd30b6
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-22 19:11:34 +0200

    6-square.py file was modified

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,7 +58,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for l in range(self.__position[1]):
+            for i in range(self.__position[1]):
                 print()
             for i in range(self.__size):
                 for s in range(self.__position[0]):
