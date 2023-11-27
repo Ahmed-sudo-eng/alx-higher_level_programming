ed8e5d1121e95a6964556f3ed4d321f646237a32
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-27 08:02:18 +0200

    0x08-python-more_classes directory, README.md and 0-rectangle.py files were created

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """An empty class Rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+0-rectangle.py: a modue that contains an empty class Rectangle that defines a recatangle.
