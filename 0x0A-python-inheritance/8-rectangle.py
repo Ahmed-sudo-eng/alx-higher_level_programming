9d654d2b29d302d728ff93454b6b941860809f37
Ahmed Omer <ahmedomer695@gmail.com>
2023-12-29 14:19:10 +0200

    8-rectangle.py file was modified

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,9 +7,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ This class inherits from BaseGeometry """
-    def __init__(self, weight, height):
+    def __init__(self, width, height):
         """ Initialize the properties """
-        super().integer_validator("weight", weight)
+        super().integer_validator("width", weight)
         super().integer_validator("height", height)
-        self.__weight = weight
+        self.__width = width
         self.__height = height
