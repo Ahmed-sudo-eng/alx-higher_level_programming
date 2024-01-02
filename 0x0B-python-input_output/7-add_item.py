52e1cbeddc68a7d1e5f0de900e5598a4c25f6793
Ahmed Omer <ahmedomer695@gmail.com>
2024-01-02 20:12:18 +0200

    7-add_item.py file was modified

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
-This module contains a function "load_from_json_file" which creates an object
-from a "JSON" file
+This module adds all arguments to a python list, and then save them to a file
 """
 import sys
-#save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-#load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 my_list = []
-#old_list = load_from_json_file('add_item.json')
+old_list = load_from_json_file('add_item.json')
 for item in old_list:
-    """ for list is very powerfull """
     my_list.append(item)
 for arg in sys.argv:
-    """ for list is very powerfull """
     if (arg == sys.argv[0]):
         pass
     else:
         my_list.append(arg)
-
-#save_to_json_file(my_list, 'add_item.json')
+save_to_json_file(my_list, 'add_item.json')
