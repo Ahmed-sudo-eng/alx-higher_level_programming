20d8fbbd788e2ac70924ffcff6a1e93313587e51
Ahmed Omer <ahmedomer695@gmail.com>
2024-01-02 18:24:39 +0200

    7-add_item.py file was modified

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
+"""
 my_list = []
 old_list = load_from_json_file('add_item.json')
 for item in old_list:
@@ -19,3 +19,4 @@ for arg in sys.argv:
         my_list.append(arg)
 
 save_to_json_file(my_list, 'add_item.json')
+"""
