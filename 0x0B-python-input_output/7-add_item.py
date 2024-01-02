adfb3938c38984061cfd5c0dfbce64f1560b7d8b
Ahmed Omer <ahmedomer695@gmail.com>
2024-01-02 18:07:39 +0200

    7-add_item.py file was modified

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,9 @@
 This module contains a function "load_from_json_file" which creates an object
 from a "JSON" file
 """
-import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def load_from_json_file(filename):
